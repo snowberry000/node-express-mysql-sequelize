23cd8104017f11fb00686c00c1e556bf1c411aa7
whitecodebear <denisvaran@outlook.com>
2019-10-30 03:01:26 +0200

    delete quote & invoices when booking deleted, fix get all invoices function

--- controllers/booking.controller.js
+++ controllers/booking.controller.js
@@ -175,7 +175,15 @@ const remove = async function(req, res){
     booking = req.booking;
 
     [err, booking] = await to(booking.destroy());
-    if(err) return ReE(res, 'error occured trying to delete the booking');
+    if(err) return ReE(res, 'error occured trying to delete the booking');    
+
+    // delete related invoices
+    [errInvoice, invoces] = await to(Invoice.destroy({where: {BookingId: booking.id}}));
+    if(errInvoice) return ReE(res, errInvoice, 422);
+
+    // delete relalted quotes
+    [errQuotes, quotes] = await to(Quote.destroy({where: {BookingId: booking.id}}));
+    if(errQuotes) return ReE(res, errQuotes, 422);
 
     return ReS(res, {message:'Deleted Booking'}, 204);
 }
@@ -353,15 +361,20 @@ const getInvoices = async function(req, res){
 }
 module.exports.getInvoices = getInvoices;
 
-const getAllInvoices = async function(req, res){
-    let err, invoices;
+const getAllInvoices = async function(req, res){    
 
-    [err, invoices] = await to(Invoice.findAll());
-    if(err) return ReE(res, err, 422);
+    let user_json = req.user.toWeb();
+    let err, bookings;
+    [err, bookings] = await to(Booking.findAll({where: {UserId: user_json.id}}));
+
+    let bookingIds = bookings.map(item => item.id)
+
+    let errInvoice, invoices;
+    [errInvoice, invoices] = await to(Invoice.findAll({where:{BookingId: bookingIds}}));
+    if(errInvoice) return ReE(res, errInvoice, 422);
 
     let invoice_array = [];
-    invoice_array = invoices.map(obj=>obj.toWeb());
-
+    invoice_array = invoices.map(obj=>obj.toWeb());    
     return ReS(res, {invoices:invoice_array});
 }
 module.exports.getAllInvoices = getAllInvoices;
